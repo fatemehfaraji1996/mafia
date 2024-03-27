540093bf2ed8a4f40b6824cb19da0a9f8200005c
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-03-27 22:12:49 +0330

    handel form

---
+++ index.js
@@ -0,0 +1,24 @@
+const inputEle = document.querySelector(".input");
+const buttonAddEle = document.querySelector(".add");
+const cantanerEle = document.querySelector('.contaner')
+// console.log(cantanerEle);
+// console.log(buttonAddEle);
+// console.log(inputEle);
+const formEle = document.querySelector("form");
+
+formEle.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const inputEle = document.querySelector(".input");
+    console.log(inputEle.value);
+    if (inputEle.value) {
+        const boxPerson = document.createElement('div')
+        formEle.append(boxPerson)
+        boxPerson.classList.add('boxPerson')
+        const pEle = document.createElement('p')
+        pEle.textContent=inputEle.value
+        boxPerson.append(pEle)
+        
+    }
+    
+});
+
