f2c8f62e3729ac0c9acd86828475d938edbbc5e2
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-03-30 08:48:26 +0330

    start

--- index.js
+++ index.js
@@ -6,16 +6,20 @@
 </div> */
 
 const form = document.querySelector('form')
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const inputEle = document.querySelector(".form-control");
-  const buttonAddEle = document.querySelector(".btn-outline-secondary");
+  const inputEle = document.querySelector('.form-control');
+  const buttonAddEle = document.querySelector('.btn-outline-secondary');
   const form = document.querySelector('form')
-  form.innerHTML += `<div class="input-group">
-  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username with two button addons">
-  <button class="btn btn-outline-secondary" type="button">Edit</button>
-  <button class="btn btn-outline-secondary" type="button">Delet</button>
-</div>`
+  // start
+  const divparentBox = document.createElement('div')
+  form.append(divparentBox)
+  divparentBox.classList.add('input-group')
+  // creat box parent
+  const inputNames = document.createElement('input')
+  divparentBox.append(inputNames)
+  inputNames.classList.add('form-control')
+  // creat input names of players
   
 
 });
